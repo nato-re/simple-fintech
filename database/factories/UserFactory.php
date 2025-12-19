faecb5be70a855565a3b263d72fc0be0096232d3
nato-re <alvare.renato@gmail.com>
2025-12-19 17:27:27 -0300

    Install Faker and fix cpf on factory

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -6,6 +6,8 @@ use App\Enums\Role;
 use Illuminate\Database\Eloquent\Factories\Factory;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Str;
+use Faker\Factory as Faker;
+
 
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\User>
@@ -24,9 +26,11 @@ class UserFactory extends Factory
      */
     public function definition(): array
     {
+        $faker = Faker::create('pt_BR');
         return [
             'name' => fake()->name(),
             'email' => fake()->unique()->safeEmail(),
+            'cpf' => $faker->unique()->cpf(),
             'email_verified_at' => now(),
             'password' => static::$password ??= Hash::make('password'),
             'remember_token' => Str::random(10),
